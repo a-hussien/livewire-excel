722c3577f8556575195d05e060ea5541b13bc3de
ahmed hussien <a.hussien87@gmail.com>
2022-01-17 18:42:19 +0200

    Create product model

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name(),
+            'brand' => $this->faker->company(),
+            'price' => $this->faker->randomFloat(2, 500,50000)
+        ];
+    }
+}
